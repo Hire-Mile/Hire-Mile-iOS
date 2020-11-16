51ea05e5cd526353dc1467c64229ae41bbd5632f
Jorge Zapata <jorgejaden@gmail.com>
2020-11-16 09:30:06 +0100

    finished onboarding

---
+++ HireMile/Helpers.swift
@@ -0,0 +1,14 @@
+//
+//  Helpers.swift
+//  HireMile
+//
+//  Created by JJ Zapata on 11/16/20.
+//  Copyright © 2020 Jorge Zapata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var mainBlue = UIColor(red: 120/255, green: 196/255, blue: 248/255, alpha: 1)
+}
