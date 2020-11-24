08823c26210fcb61507e49fa1c1030ab6cc2ad1d
Jorge Zapata <jorgejaden@gmail.com>
2020-11-24 19:33:38 +0100

    finished biometrics entrance

---
+++ HireMile/Tabs/Menu Bar/Favorites/Favorites.swift
@@ -0,0 +1,154 @@
+//
+//  Favorites.swift
+//  HireMile
+//
+//  Created by JJ Zapata on 11/23/20.
+//  Copyright © 2020 Jorge Zapata. All rights reserved.
+//
+
+import UIKit
+
+class Favorites: UITableViewController {
+    
+    private let refrshControl : UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor.black
+        refreshControl.addTarget(self, action: #selector(refreshAction), for: .valueChanged)
+        return refreshControl
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = true
+        tableView.register(FavoritesCell.self, forCellReuseIdentifier: "favoritesCellID")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Functions to throw
+        self.basicSetup()
+    }
+    
+    func basicSetup() {
+        self.view.backgroundColor = UIColor.white
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.topItem?.title = "Favorites"
+        self.navigationController?.navigationBar.tintColor = UIColor.mainBlue
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 247/255, green: 248/255, blue: 248/255, alpha: 1)
+        self.navigationController?.navigationBar.setBackgroundImage(nil, for: .bottom, barMetrics: .default)
+        self.navigationController?.navigationBar.shadowImage = nil
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.allowsSelection = true
+        self.tableView.refreshControl = refrshControl
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favoritesCellID", for: indexPath) as! FavoritesCell
+        cell.profileImageView.backgroundColor = .green
+        cell.profileImageView.image = UIImage(named: "profilepic")
+        cell.textLabel?.text = "Jorge Zapata"
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        cell.detailTextLabel?.isHidden = false
+        cell.detailTextLabel?.textColor = UIColor.darkGray
+        cell.detailTextLabel?.text = "iOS Developer"
+        cell.favoriteButton.imageView?.contentMode = .scaleAspectFill
+        cell.favoriteButton.addTarget(self, action: #selector(unheartTapped), for: .touchUpInside)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.navigationController?.pushViewController(OtherProfile(), animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    @objc func unheartTapped() {
+        let alert = UIAlertController(title: "Remove from favorites?", message: "Are you want to remove Jorge Zapata?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+            print("deleted")
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func refreshAction() {
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(hideRefrsh), userInfo: nil, repeats: false)
+    }
+    
+    @objc func hideRefrsh() {
+        self.refrshControl.endRefreshing()
+    }
+
+}
+
+class FavoritesCell: UITableViewCell {
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 75, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        detailTextLabel?.frame = CGRect(x: 75, y: detailTextLabel!.frame.origin.y - 2, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let favoriteButton : UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "heart.fill"), for: .normal)
+        button.tintColor = UIColor.mainBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.imageView?.contentMode = .scaleAspectFill
+        button.clipsToBounds = true
+        button.imageView?.tintColor = UIColor.mainBlue
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        addSubview(favoriteButton)
+        
+        //ios 9 constraint anchors
+        //need x,y,width,height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
+        favoriteButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10).isActive = true
+        favoriteButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        favoriteButton.widthAnchor.constraint(equalToConstant: 35).isActive = true
+        favoriteButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
