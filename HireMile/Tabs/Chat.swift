8e5b1a4758cdfc867dfb7739d540c23bcf0821ba
Jorge Zapata <jorgejaden@gmail.com>
2020-12-01 19:12:57 +0100

    finished notifcations on frontend along with handling actions on tap

--- HireMile/Tabs/Chat.swift
+++ HireMile/Tabs/Chat.swift
@@ -8,25 +8,192 @@
 
 import UIKit
 
-class Chat: UIViewController {
+class Chat: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let titles = ["Cash Out", "Cash In"]
+    let descriptions = ["You have cashed out $253 to your wallet from HireMile", "You have cashed in $500 to your HireMile wallet"]
+        
+    private let refrshControl : UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor.black
+        refreshControl.addTarget(self, action: #selector(refreshAction), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    let segmentedControl : UISegmentedControl = {
+        let segmentedControl = UISegmentedControl()
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.insertSegment(withTitle: "Messages", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "Notifications", at: 1, animated: true)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.layer.cornerRadius = 20
+        segmentedControl.layer.borderColor = UIColor(red: 241/255, green: 239/255, blue: 239/255, alpha: 1).cgColor
+        segmentedControl.layer.borderWidth = 2
+        segmentedControl.layer.masksToBounds = true
+        segmentedControl.backgroundColor = UIColor.mainBlue
+        segmentedControl.setBackgroundImage(UIImage(named: "whiteback"), for: .normal, barMetrics: .default)
+        segmentedControl.setBackgroundImage(UIImage(named: "mainBlue"), for: .selected, barMetrics: .default)
+        segmentedControl.tintColor = UIColor.mainBlue
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1)], for: .normal)
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        return segmentedControl
+    }()
+    
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = UIColor.white
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(segmentedControl)
+        self.segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true
+        self.segmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.segmentedControl.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        self.segmentedControl.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        self.view.addSubview(tableView)
+        self.tableView.topAnchor.constraint(equalTo: self.segmentedControl.bottomAnchor, constant: 20).isActive = true
+        self.tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        self.tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
         self.view.backgroundColor = UIColor.white
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.refreshControl = refrshControl
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: "myNotificationsCellID")
+        tableView.register(MessagesCellCell.self, forCellReuseIdentifier: "myMessagesCellID")
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.view.backgroundColor = UIColor.white
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.topItem?.title = "Chat"
+        self.navigationController?.navigationBar.tintColor = UIColor.mainBlue
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            return 2
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            return 2
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myMessagesCellID", for: indexPath) as! MessagesCellCell
+            return cell
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myNotificationsCellID", for: indexPath) as! NotificationCell
+            cell.profileImageView.image = UIImage(named: "ProfileIcon")
+            cell.textLabel?.text = titles[indexPath.row]
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+            cell.detailTextLabel?.isHidden = false
+            cell.detailTextLabel?.textColor = UIColor.darkGray
+            cell.detailTextLabel?.text = descriptions[indexPath.row]
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myJobsCellID", for: indexPath) as! MyJobsRunningCell
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            return 110
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            return 90
+        } else {
+            return 125
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            print("open conversaton")
+        } else if segmentedControl.selectedSegmentIndex == 1 {
+            let alert = UIAlertController(title: titles[indexPath.row], message: descriptions[indexPath.row], preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+        }
+    }
+    
+    @objc func refreshAction() {
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(hideRefrsh), userInfo: nil, repeats: false)
+    }
+    
+    @objc func hideRefrsh() {
+        self.refrshControl.endRefreshing()
+    }
+    
+    @objc func segmentedControlValueChanged(_ semder: UISegmentedControl) {
+        self.tableView.reloadData()
+    }
 
+}
+
+class NotificationCell: UITableViewCell {
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 75, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        detailTextLabel?.frame = CGRect(x: 75, y: detailTextLabel!.frame.origin.y - 2, width: detailTextLabel!.frame.width - 30, height: detailTextLabel!.frame.height)
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 24
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        //ios 9 constraint anchors
+        //need x,y,width,height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class MessagesCellCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
